a53b26105a3a49181c8f6e1f02b0e20fd20f7deb
Jugya K. Gogoi <crjugya123@gmail.com>
2022-12-03 00:01:13 +0530

    updated and added BST

--- TreeDS/binarySearchTree.c
+++ TreeDS/binarySearchTree.c
@@ -1,3 +1,7 @@
+//nodes of the left subtree is lesser
+//nodes of the right subtree is greater
+//no duplicate nodes
+//inorder traversal of bst gives an ascending sorted array
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -42,22 +46,41 @@ void inorder(struct node *root){
     }
 }
 
+//is the tree BST
+int isBST(struct node *root){
+   static  struct node *prev = NULL;
+    if(root!=NULL){
+        if(!isBST(root->left)){
+             return 0;
+        }
+        if(prev != NULL && root->data <= prev->data){
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }else{
+        return 1;
+    }
+}
+
+
 int main(){
-    struct node *root = newNode(1);
-    struct node *p1 = newNode(2);
-    struct node *p2 = newNode(3);
+    struct node *root = newNode(10);
+    struct node *p1 = newNode(4);
+    struct node *p2 = newNode(15);
     root->left = p1;
     root->right = p2;
-    p1->left = newNode(4);
-    p1->right = newNode(5);
-    p2->left = newNode(6);
-    p2->right = newNode(7);
+    p1->left = newNode(1);
+    p1->right = newNode(6);
+    p2->left = newNode(11);
+    p2->right = newNode(16);
    
     preorder(root);
     printf("\n");
     postorder(root);
     printf("\n");
-    inorder(root);    
+    inorder(root);
+    printf("\n%d ",isBST(root));    
    
     return 0;
 }
